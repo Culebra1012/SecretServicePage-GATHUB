f17d9516c1e41f4696f32ea8b0ffb8f69309860d
SebastiÃan Duque <duque232015@gmail.com>
2022-08-07 02:51:41 -0500

    se sube el archivo

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.swiper = new Swiper({
+    el: '.slider-contenedor',
+    slideClass: 'slider-item',
+    createElements: true,
+    autoplay: {
+        delay: 3000
+    },
+    loop: true,
+    pagination: true,
+    navigation: true
+});
